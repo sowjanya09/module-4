efca04812848a7c964d4b563a978697264dee2c5
sowjanya09 <51982428+sowjanya09@users.noreply.github.com>
2020-06-17 11:26:06 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,6 +1,7 @@
 (function(window){
+var helloSpeaker = {};
 var speakWord="Hello";
-var helloSpeaker = function(name){
+helloSpeaker.speak=function(name){
 console.log(speakWord+" "+name);
 }
 window.helloSpeaker = helloSpeaker;
