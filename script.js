bc9663997adbb1aaf0b8680e2534a67ea6d241ca
sowjanya09 <51982428+sowjanya09@users.noreply.github.com>
2020-06-17 11:50:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-(function(window){
-var names=["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
-for(var i in names){
-var firstLetter = ((names[i]).char At(0)).toLowerCase();
-if(firstletter =='j'){
-window.byeSpeaker.speak(names[i]);
+(function (window) {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for(var i = 0; i < names.length; i++){
+var firstLetter = (names[i].charAt(0).toLowerCase();
+if (firstletter =='j') {
+  window.byeSpeaker.speak(names[i]);
 } else{
-window.helloSpeaker.speak(names[i]);
+  window.helloSpeaker.speak(names[i]);
 }
 }
 })(window);
