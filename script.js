6d8892fe83262aae6d56f2b485d7b3c41b06f8c5
sowjanya09 <51982428+sowjanya09@users.noreply.github.com>
2020-06-17 11:54:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function (window) {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for(var i = 0; i < names.length; i++){
-var firstLetter = (names[i].charAt(0).toLowerCase();
+var firstLetter = names[i].charAt(0).toLowerCase();
 if (firstletter =='j') {
   window.byeSpeaker.speak(names[i]);
 } else{
